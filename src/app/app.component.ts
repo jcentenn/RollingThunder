04eeca761cfd53aab2084d2b48135ae27f03ad8a
Joe Centenni <jcentenn@panix.com>
2018-12-05 21:49:53 -0500

    menus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from "./app.service";
 import { Title } from "@angular/platform-browser";
+import { MenuItem } from 'primeng/api';
 
 
 @Component({
@@ -10,16 +11,34 @@ import { Title } from "@angular/platform-browser";
 })
 
 export class AppComponent implements OnInit {
-  title = 'Rolling Thunder';
+//  title = 'Rolling Thunder';
 //  title = '.';
   data: string;
   
+    items: MenuItem[];
+
   constructor(private appService: AppService, private titleService: Title ) { }
   
   ngOnInit() {
 //      this.showData();
-      this.appService.changeFavicon('https://royaleapi.com/static/img/badge/gold-3/Bolt_03.png');
-      this.setTitle('Rolling Thunder');
+//      this.appService.changeFavicon('https://royaleapi.com/static/img/badge/gold-3/Bolt_03.png');
+//      this.setTitle('Rolling Thunder');
+      
+      this.items = [{
+          label: 'File',
+          items: [
+              {label: 'New', icon: 'pi pi-fw pi-plus'},
+              {label: 'Download', icon: 'pi pi-fw pi-download'}
+          ]
+      },
+      {
+          label: 'Edit',
+          items: [
+              {label: 'Add User', icon: 'pi pi-fw pi-user-plus'},
+              {label: 'Remove User', icon: 'pi pi-fw pi-user-minus'}
+          ]
+      }];
+      
     }
   
   setTitle( newTitle: string) {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppService } from './app.service';
+import { MenuModule } from 'primeng/menu';
+import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+import { TabViewModule } from 'primeng/tabview';
+import { CodeHighlighterModule } from 'primeng/codehighlighter';
 
 
 @NgModule({
@@ -12,7 +18,12 @@ import { AppService } from './app.service';
   ],
   imports: [
     BrowserModule
+    , BrowserAnimationsModule
     , HttpClientModule
+    , MenuModule
+    , MenubarModule
+    , ButtonModule
+    , CodeHighlighterModule
   ],
   providers: [AppService, Title],
   bootstrap: [AppComponent]
